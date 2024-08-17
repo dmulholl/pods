d0dfc80107f3741a6911428b720a6f58c8dbdd56
Darren Mulholland <dmulholl@tcd.ie>
2024-08-17 10:20:33 +0100

    Change the template string format

--- cmd/pods/main.go
+++ cmd/pods/main.go
@@ -18,7 +18,7 @@ import (
 	"github.com/dmulholl/pods/internal/term"
 )
 
-const version = "v0.1.0"
+const version = "v0.2.0"
 
 var helptext = fmt.Sprintf(`
 Pods %s
@@ -32,12 +32,10 @@ Description:
   By default, this utility simply lists the episodes which would be downloaded.
   Use the -d/--download flag to actually download the episodes.
 
-  The --after/--before options accept a simple date, e.g.
+  The --before and --after options accept a simple date or a full timestamp in
+  RFC-3339 format, e.g.
 
     --after "2024-07-31"
-
-  Or, a full RFC-3339 timestamp, e.g.
-
     --after "2024-07-31T13:59:00+02:00"
 
   If no timezone offset is specified, the timestamp is assumed to be UTC.
@@ -45,14 +43,14 @@ Description:
   The output filename can be customized using the -f/--format option. The
   following format specifiers are supported:
 
-  - %%episode%%:  Episode number.
-  - %%episode2%%: Episode number with zero-padding, min-width: 2 digits.
-  - %%episode3%%: Episode number with zero-padding, min-width: 3 digits.
-  - %%episode4%%: Episode number with zero-padding, min-width: 4 digits.
-  - %%ext%%:      The default file extension for the file type, e.g. '.mp3'.
-  - %%title%%:    The episode title.
+  - {{episode}}:  Episode number.
+  - {{episode2}}: Episode number with zero-padding, min-width: 2 digits.
+  - {{episode3}}: Episode number with zero-padding, min-width: 3 digits.
+  - {{episode4}}: Episode number with zero-padding, min-width: 4 digits.
+  - {{ext}}:      The default file extension for the file type, e.g. '.mp3'.
+  - {{title}}:    The episode title.
 
-  The default filename format is '%%episode4%%. %%title%%%%ext%%'.
+  The default filename format is '{{episode4}}. {{title}}{{ext}}'.
 
 Options:
   -a, --after <timestamp>   Download episodes published after this timestamp.
@@ -61,7 +59,7 @@ Options:
                             This option can be specified multiple times.
       --file <filepath>     Specifies a source file for the RSS feed.
   -f, --format <format>     Overrides the default format for output filenames.
-                            Default: '%%episode4%%. %%title%%%%ext%%'.
+                            Default: '{{episode4}}. {{title}}{{ext}}'.
   -o, --outdir <path>       Output directory for downloaded files.
                             Default: './<podcast-title>'.
   -u, --url <url>           Specifies a source URL for the RSS feed.
@@ -91,7 +89,7 @@ func main() {
 	argparser.NewStringOption("after a", "")
 	argparser.NewStringOption("outdir o", "")
 	argparser.NewIntOption("episode e", 0)
-	argparser.NewStringOption("format f", "%episode4%. %title%%ext%")
+	argparser.NewStringOption("format f", "{{episode4}}. {{title}}{{ext}}")
 	argparser.NewFlag("download d")
 	argparser.NewFlag("quiet q")
 
@@ -322,21 +320,21 @@ func downloadEpisodeQuietly(ctx context.Context, client *http.Client, filepath s
 }
 
 func formatFilename(format string, episode rss.Item) (string, error) {
-	filename := strings.Replace(format, "%title%", strings.TrimSpace(episode.Title), -1)
+	filename := strings.Replace(format, "{{title}}", strings.TrimSpace(episode.Title), -1)
 
-	if strings.Contains(filename, "%ext%") {
+	if strings.Contains(filename, "{{ext}}") {
 		ext, err := extensionForType(episode.Enclosure.Type)
 		if err != nil {
 			return "", fmt.Errorf("failed to determine the default file extension: %w", err)
 		}
 
-		filename = strings.Replace(filename, "%ext%", ext, -1)
+		filename = strings.Replace(filename, "{{ext}}", ext, -1)
 	}
 
-	filename = strings.Replace(filename, "%episode%", fmt.Sprintf("%d", episode.Episode), -1)
-	filename = strings.Replace(filename, "%episode2%", fmt.Sprintf("%02d", episode.Episode), -1)
-	filename = strings.Replace(filename, "%episode3%", fmt.Sprintf("%03d", episode.Episode), -1)
-	filename = strings.Replace(filename, "%episode4%", fmt.Sprintf("%04d", episode.Episode), -1)
+	filename = strings.Replace(filename, "{{episode}}", fmt.Sprintf("%d", episode.Episode), -1)
+	filename = strings.Replace(filename, "{{episode2}}", fmt.Sprintf("%02d", episode.Episode), -1)
+	filename = strings.Replace(filename, "{{episode3}}", fmt.Sprintf("%03d", episode.Episode), -1)
+	filename = strings.Replace(filename, "{{episode4}}", fmt.Sprintf("%04d", episode.Episode), -1)
 
 	return filename, nil
 }
